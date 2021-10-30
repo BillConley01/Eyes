3e9518156acb9b918db8b7fc8bc496c455a56d92
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-10-29 22:59:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var balls = document.getElementsByClassName("ball");
-document.onmousemove = () => {
-  var x = (event.clientX * 100) / window.innerWidth + "%";
-  var y = (event.clientY * 100) / window.innerHeight + "%";
+let balls = document.getElementsByClassName("ball");
+
+document.addEventListener('mousemove',  e => {
+  let x = (e.clientX * 100) / window.innerWidth +'%';
+  let y = (e.clientY * 100) / window.innerHeight + '%';
+  
 
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
-    balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
+    balls[i].transform = "translate(-" + x + ",-" + y + ")";
+    
   }
-};
+  
+
+});
